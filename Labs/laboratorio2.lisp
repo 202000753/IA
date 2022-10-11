f0e73939a97b7b843daa64d08df1838ce6b1bf54
202000753 <202000753@estudantes.ips.pt>
2022-10-11 21:22:29 +0100

    11-10

---
+++ Labs/laboratorio2.lisp
@@ -0,0 +1,17 @@
+;;;; laboratorio2.lisp
+;;;; Disciplina de IA - 2022 / 2023
+;;;; Ficha de Laboratório nº2
+;;;; Autor: 
+
+;;; Para Debug
+;; soma-3
+(defun soma-3 (a b c)
+"Função que faz a soma de 3 números passados como argumento"
+	(+ a b c)
+)
+
+;;; Exercicios sobre funções em Lisp (não recursivas)
+;; notas-dos-alunos
+;; notas-do-primeiro-aluno
+;; calcula-media-notas
+;; maior-nota-do-aluno
